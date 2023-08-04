6f7de2015057edbe9a8f293e30bd304addd97886
Mustafa <mustafataibah@gmail.com>
2023-08-04 03:06:40 +0300

    ExerciseCard

--- components/ExerciseCard.js
+++ components/ExerciseCard.js
@@ -1,14 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import SetCard from "./SetCard";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+  Dimensions,
+} from "react-native";
 import { createUUID } from "../utils/generateUUID";
+import Feather from "react-native-vector-icons/Feather";
+import SetCard from "./SetCard";
 
 const ExerciseCard = ({ exercise, handleAddSet, handleDeleteSet }) => {
   const [sets, setSets] = useState(exercise.sets || []);
+  const [showIcon, setShowIcon] = useState(false);
+  const animatedValue = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     handleAddSet(exercise.id, sets);
   }, [sets]);
+
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: showIcon ? 1 : 0,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  }, [showIcon]);
 
   const handleSetChange = (index, weight, reps) => {
     const newSets = [...sets];
@@ -17,6 +35,7 @@ const ExerciseCard = ({ exercise, handleAddSet, handleDeleteSet }) => {
   };
 
   const handleAddSetLocal = () => {
+    if (showIcon) return;
     setSets((prevSets) => {
       const newSets = [
         ...prevSets,
@@ -31,52 +50,99 @@ const ExerciseCard = ({ exercise, handleAddSet, handleDeleteSet }) => {
     });
   };
 
+  const backgroundColorInterpolation = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["#151919", "#0f0e0e"],
+  });
+
+  const animatedStyle = {
+    backgroundColor: backgroundColorInterpolation,
+  };
+
   return (
-    <View key={exercise.id} style={styles.card}>
-      <Text style={styles.cardTitle}>{exercise.name}</Text>
-      {exercise.sets.map((set, index) => (
-        <SetCard
-          key={set.id}
-          data={set}
-          setNumber={index + 1}
-          // Ong chatgpt the goat Ion even know what the fuck this code does but it works
-          onDelete={() => {
-            handleDeleteSet(exercise.id, set.id);
-            setSets(sets.filter((s) => s.id !== set.id));
-          }}
-          onSetChange={(weight, reps) => handleSetChange(index, weight, reps)}
-        />
-      ))}
-      <TouchableOpacity style={styles.addSetButton} onPress={handleAddSetLocal}>
-        <Text style={styles.addSetText}>Add Set</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity
+      activeOpacity={1}
+      onPress={() => setShowIcon(false)}
+      onLongPress={() => setShowIcon(true)}>
+      <Animated.View style={[styles.card, animatedStyle]}>
+        <View style={styles.header}>
+          <Text style={styles.cardTitle}>{exercise.name}</Text>
+          {showIcon && (
+            <TouchableOpacity
+              style={styles.trashIcon}
+              onPress={() => handleDeleteExercise(exercise.id)}>
+              <Feather name="trash-2" size={24} color="#CE2029" />
+            </TouchableOpacity>
+          )}
+        </View>
+        {exercise.sets.map((set, index) => (
+          <SetCard
+            key={set.id}
+            data={set}
+            setNumber={index + 1}
+            onDelete={() => {
+              handleDeleteSet(exercise.id, set.id);
+              setSets(sets.filter((s) => s.id !== set.id));
+            }}
+            onSetChange={(weight, reps) => handleSetChange(index, weight, reps)}
+          />
+        ))}
+        <TouchableOpacity
+          style={[styles.addSetButton, showIcon && styles.disabledButton]}
+          onPress={handleAddSetLocal}
+          activeOpacity={showIcon ? 1 : 0.2}>
+          <Text style={styles.addSetText}>Add Set</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: "#000",
+    backgroundColor: "#151919",
     borderRadius: 10,
     padding: 20,
     marginBottom: 20,
+    marginHorizontal: 10,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
   },
   cardTitle: {
-    color: "#fff",
+    color: "#FFFAFA",
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: "700",
+    marginBottom: 10,
+    lineHeight: 24,
+  },
+  trashIcon: {
+    view: "hidden",
+    alignSelf: "center",
     marginBottom: 10,
   },
   addSetButton: {
+    alignSelf: "center",
+    width: "30%",
     alignItems: "center",
     padding: 10,
-    borderRadius: 5,
-    borderColor: "#fff",
+    backgroundColor: "#D7F2F2",
+    borderRadius: 10,
+    borderColor: "#FFFAFA",
     borderWidth: 1,
-    marginTop: 10,
+    marginTop: 20,
+  },
+  disabledButton: {
+    backgroundColor: "#323334",
+    borderWidth: 0,
   },
   addSetText: {
-    color: "#fff",
+    color: "#151919",
+    fontSize: 14,
+    fontWeight: "700",
   },
 });
 

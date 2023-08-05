12784aa3682f58d9221dce10ca4240c4539a5a7c
Mustafa <mustafataibah@gmail.com>
2023-08-05 19:11:10 +0300

    removed shaking animation

--- components/Box.js
+++ components/Box.js
@@ -1,62 +1,26 @@
-import React, { useState, useRef } from "react";
-import {
-  TouchableOpacity,
-  Text,
-  Alert,
-  StyleSheet,
-  Animated,
-  Easing,
-} from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity, Text, StyleSheet, Modal, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 const Box = ({ box, isLastBox, handleGoToWorkoutView, onDeleteBox }) => {
   const [longPress, setLongPress] = useState(false);
-  const shakeAnimation = useRef(new Animated.Value(0)).current;
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
 
   const onLongPress = () => {
-    setLongPress(!longPress);
-    longPress ? shakeAnimation.stopAnimation() : startShake();
-    if (!longPress) {
-      Alert.alert("Delete Workout", "Are you sure you want to delete this workout?", [
-        {
-          text: "Cancel",
-          onPress: () => {
-            setLongPress(false);
-            shakeAnimation.setValue(0);
-            shakeAnimation.stopAnimation();
-          },
-        },
-        {
-          text: "OK",
-          onPress: () => {
-            if (onDeleteBox) {
-              onDeleteBox();
-              setLongPress(false);
-              shakeAnimation.stopAnimation();
-            }
-          },
-        },
-      ]);
+    setLongPress(true);
+    setDeleteModalVisible(true);
+  };
+
+  const handleDelete = () => {
+    if (onDeleteBox) {
+      onDeleteBox();
+      cancelLongPress();
     }
   };
 
-  const startShake = () => {
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(shakeAnimation, {
-          toValue: 10,
-          duration: 100,
-          useNativeDriver: true,
-          easing: Easing.linear,
-        }),
-        Animated.timing(shakeAnimation, {
-          toValue: -10,
-          duration: 100,
-          useNativeDriver: true,
-          easing: Easing.linear,
-        }),
-      ])
-    ).start();
+  const cancelLongPress = () => {
+    setLongPress(false);
+    setDeleteModalVisible(false);
   };
 
   const containerStyle = isLastBox
@@ -69,23 +33,43 @@ const Box = ({ box, isLastBox, handleGoToWorkoutView, onDeleteBox }) => {
       onPress={handleGoToWorkoutView}
       onLongPress={onLongPress}
       delayLongPress={1000}>
-      <Animated.View
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={deleteModalVisible}
+        onRequestClose={cancelLongPress}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Do you want to delete the workout?</Text>
+            <View style={styles.modalButtons}>
+              <TouchableOpacity style={styles.confirmDeleteButton} onPress={handleDelete}>
+                <Text style={styles.modalButtonText}>Delete</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.noButton} onPress={cancelLongPress}>
+                <Text style={styles.modalButtonText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <View
         style={{
-          transform: [{ translateX: shakeAnimation }],
           justifyContent: "center",
           alignItems: "center",
           flex: 1,
+          width: "80%",
         }}>
-        <Text style={styles.boxText}>{box}</Text>
         {longPress && (
           <Feather
             name="trash-2"
-            size={80}
-            color="#fff"
-            style={{ position: "absolute", top: 10 }}
+            size={24}
+            color="#151919"
+            style={{ position: "absolute", top: 10, right: 0 }}
           />
         )}
-      </Animated.View>
+        <Text style={longPress ? styles.boxTextLongPress : styles.boxText}>{box}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -94,27 +78,81 @@ const styles = StyleSheet.create({
   evenBoxContainer: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f0f0f0",
+    backgroundColor: "#1b77ee",
     width: "46%",
     aspectRatio: 1,
     margin: "2%",
-    borderRadius: 12,
+    borderRadius: 15,
   },
   oddBoxContainer: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f0f0f0",
+    backgroundColor: "#1b77ee",
     width: "96%",
     aspectRatio: 2,
     margin: "2%",
-    borderRadius: 12,
+    borderRadius: 15,
   },
   boxLongPress: {
-    backgroundColor: "red",
+    backgroundColor: "#ce2029",
   },
   boxText: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "#FFFAFA",
+  },
+  boxTextLongPress: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "#151919",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    backgroundColor: "#151919",
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  confirmDeleteButton: {
+    flex: 1,
+    margin: 10,
+    backgroundColor: "#CE2029",
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+  },
+  noButton: {
+    flex: 1,
+    margin: 10,
+    backgroundColor: "#A9A9A9",
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+  },
+  modalText: {
+    color: "#FFFAFA",
     fontSize: 16,
+    fontWeight: "700",
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  modalButtonText: {
+    color: "white",
     fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
   },
 });
 
